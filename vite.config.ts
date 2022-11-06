ccd4d360ef04412aec3eaaea975f4156f22461fb
sahokun <kurukurumawaru@gmail.com>
2022-11-06 17:18:40 +0900

    fix deploy

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,11 @@ import { fileURLToPath, URL } from "node:url";
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import vueJsx from "@vitejs/plugin-vue-jsx";
-import pluginRewriteAll from "vite-plugin-rewrite-all";
 // https://vitejs.dev/config/
 export default defineConfig({
   root: "src",
-  base: process.env.GITHUB_PAGES ? "mch-test" : "./",
-  plugins: [vue(), vueJsx(), pluginRewriteAll()],
+  base: process.env.GITHUB_PAGES ? "/mch-test/" : "/",
+  plugins: [vue(), vueJsx()],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
